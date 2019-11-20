fa3b5464f155ed4bd9c308ab8ced46d5f9a37dc2
TinyHippy <andrew.marinic92@gmail.com>
2019-11-19 23:01:29 -0600

    update

--- imgLib.py
+++ imgLib.py
@@ -5,6 +5,39 @@ import math
 # imageLibrary - Sci2000 - T01 - Andrew Marinic - 7675509
 
 
+
+# this function makes a circle matrix
+def makeCircle(matRow, matCol, radius, maxPix, xCord, yCord, black=True):
+    mat = np.zeros((matRow, matCol))
+    midX = xCord
+    midY = yCord
+    if xCord < int(radius/2) or xCord > matCol-int(radius/2):
+        midX = int(matCol/2)
+    if yCord < int(radius/2) or yCord > matRow-int(radius/2):
+        midY = int(matRow/2)
+    """
+    circledata is a 3-tuple containing the radius of the circle and the x- and
+    y- coordinates of the center of the circle.  All of these values should be
+    integers.
+    By default, the pixels making up the circle are set to 0.  If black is
+    set to False, the pixels making how to test if I can connect to serverup
+    the circle are set to MAXPIXEL.
+    """
+    for ay in range(-radius, radius):
+        # ay is the y-coordinate, measured with respect to the center
+        # of the circle.
+        # at height ay, the horizontal distance from the y-axis to the
+        # boundary of the circle is sqrt(r^2 - ay^2)
+        boundx = math.sqrt(radius*radius - ay*ay)
+        for ax in range(-radius, radius):
+            if -boundx < ax and ax < boundx:
+                if black:
+                    mat[midY+ay, midX+ax] = 0
+                else:
+                    mat[midY+ay, midX+ax] = maxPix
+    return mat
+
+
 #  uncanny edge detection algo, input a matrix and and upper and lower
 #  threshold for noise reduction returns a np.array
 def uncannyEdge(matrix, maxPix, low, high):
--- makeCircle.py
+++ makeCircle.py
@@ -1,38 +1,15 @@
-import math
-import numpy as np
 import imgLib as il
+import numpy as np
 
-
-def makeCircle(matRow, matCol, radius, maxPix, xCord, yCord, black=True):
-    mat = np.zeros((matRow, matCol))
-    midX = xCord
-    midY = yCord
-    if xCord < int(radius/2) or xCord > matCol-int(radius/2):
-        midX = int(matCol/2)
-    if yCord < int(radius/2) or yCord > matRow-int(radius/2):
-        midY = int(matRow/2)
-    """
-    circledata is a 3-tuple containing the radius of the circle and the x- and
-    y- coordinates of the center of the circle.  All of these values should be
-    integers.
-    By default, the pixels making up the circle are set to 0.  If black is
-    set to False, the pixels making how to test if I can connect to serverup
-    the circle are set to MAXPIXEL.
-    """
-    for ay in range(-radius, radius):
-        # ay is the y-coordinate, measured with respect to the center
-        # of the circle.
-        # at height ay, the horizontal distance from the y-axis to the
-        # boundary of the circle is sqrt(r^2 - ay^2)
-        boundx = math.sqrt(radius*radius - ay*ay)
-        for ax in range(-radius, radius):
-            if -boundx < ax and ax < boundx:
-                if black:
-                    mat[midY+ay, midX+ax] = 0
-                else:
-                    mat[midY+ay, midX+ax] = maxPix
-    return mat
-
-
-out = makeCircle(150, 150, 25, 255, 26, 75, black=False)
-il.makeImage("circle.pgm", "", 255, out)
+print("Pick a pgm file to make a reference library for")
+file_name = input()
+matList = il.makeMatrix(file_name)
+mat = matList[0]
+row = np.shape(mat)[1]
+col = np.shape(mat)[0]
+maxRadius = min(row, col)  # #maximum radius to fit the picture
+dec = max(1, int(maxRadius/10))
+for radius in range(dec, maxRadius, dec):
+    out = il.makeCircle(row, col, radius, matList[1],
+                        int(col/2), int(row/2), black=True)
+    il.makeImage("circle.pgm", radius, 255, out)
