95580d55c6f2f235b437431d8671d43b504e157d
TinyHippy <andrew.marinic92@gmail.com>
2019-11-28 23:55:26 -0600

    added auto_brighten and started iteration over array

--- runTrials.py
+++ runTrials.py
@@ -1,25 +1,28 @@
 import imgLib as il  # Andrew Marinic's image function library
-import imagefunctions as imf  # Andrea's Abellera's image function library
+import imagefunctions as imf  # Andrea Abellera's image function library
 import numpy as np
 import os
 
-print("Pick a pgm file to make a reference library for")
-file_name = input()
+file_name = input("Pick a pgm file to make a reference library for\n")
 print("Experiment Directory to be created if one does not exist\n")
 prj_dir = file_name[:-4] + "_Exp"  # name of the project directory
 il.makeDir(prj_dir)
 matList = il.makeMatrix(file_name)
 os.chdir(prj_dir)
-mat = matList[0]
-maxPix = matList[1]
-row = np.shape(mat)[0]
-col = np.shape(mat)[1]
+mat = imf.auto_brighten(matList[0])  # our matrix we will be testing
+maxPix = np.amax(mat)  # after brightening grabbing the new largest value
+row = np.shape(mat)[0]  # rows of test image
+col = np.shape(mat)[1]  # columns of test image
+
+# Selecting thresholds for uncanny
 print("Please select a low threshold for noise\n",
       "reduction in uncanny edge detect")
 low = int(input())
 print("Please select a high threshold for noise\n",
       "reduction in uncanny edge detect")
 high = int(input())
+# lets not make more images than we need, if the source image with the the
+# selected thresholds exsist lets just convert it to a np.array
 checkName = file_name[:-4] + '_Uncanny_' + str(low) + '_' + str(high) + ".pgm"
 print(checkName)
 if not os.path.exists(checkName):
@@ -32,6 +35,9 @@ else:
     matList = il.makeMatrix(checkName)
     mat = matList[0]
 
+
+# picking how many trials you want to do will do finer scans each trial. The
+# first trial is just looking at the image as whole
 print("Please enter the number of trials you would like to attempt.\n",
       "Each trial breaksdown an image into smaller submatrices to scan\n",
       "starting with the entire image as the first submatrix, each trial\n",
@@ -47,11 +53,19 @@ for t in range(1, trials+1):
         else:
             tcol = int(tcol/2)
     il.makeCircLib(file_name[:-4], trow, tcol, maxPix)
-    print(os.getcwd())
     if t > 1:
         print("need to iterate over sub matrices and run trials")
+        hCol = int(tcol/2)  # half column
+        hRow = int(trow/2)  # half row
+
+        for x in range(hCol, col-hCol,
+                       int(tcol/4)):
+            for y in range(hRow, row-hRow,
+                           int(trow/4)):
+                submat = mat[y-hRow:y+hRow, x-hCol:x+hCol]
+                imf.calcD(submat, maxPix, os.getcwd())
+        os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
     else:
         imf.calcD(mat, maxPix, os.getcwd())
     os.chdir(os.path.normpath(os.getcwd() + os.sep + os.pardir))
-    print(os.getcwd())
 print("~~~~~~~~~~~~Process Completed~~~~~~~~~~~~")
